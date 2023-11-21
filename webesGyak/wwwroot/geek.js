84fa44dfacb168c6b39be4ea1f9ae0e9563f5f09
matyasbig <matyas.verseghi-nagy@stud.uni-corvinus.hu>
2023-11-21 10:03:27 +0100

    vicc listázás

---
+++ webesGyak/wwwroot/geek.js
@@ -0,0 +1,19 @@
+﻿var jokes;
+
+window.onload = download();
+
+function download() {
+    fetch("/jokes.json")
+        .then(response => response.json()).then(data => completeDownload(data))
+}
+function completeDownload(d) {
+    console.log("Success.");
+    console.log(d)
+    jokes = d;
+    for (var i = 0; i < jokes.length;i++) {
+        console.log(jokes[i].answer);
+        var line = document.createElement("div");
+        line.innerHTML = `<p>${jokes[i].question}</p><p>${jokes[i].answer}</p>`;
+        document.body.appendChild(line);
+    }
+}
