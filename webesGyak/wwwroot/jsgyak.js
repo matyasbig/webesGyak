f80808d60f32aa650241f0d9c96d3d8fc5af9bba
matyasbig <matyas.verseghi-nagy@stud.uni-corvinus.hu>
2023-11-21 09:32:26 +0100

    pascal-háromszög hozzáadása

---
+++ webesGyak/wwwroot/jsgyak.js
@@ -0,0 +1,44 @@
+﻿window.onload = function () {
+    createRows();
+}
+    
+function createRows() {
+    var parentDiv = document.createElement("div");
+    parentDiv.id = "pascal";
+    document.body.appendChild(parentDiv);
+
+    // rows
+    for (var n = 0; n < 10; n++) {
+        var row = document.createElement("div");
+
+        row.classList.add("row");
+
+        parentDiv.appendChild(row);
+
+        // values
+        for (var k = 0; k <= n; k++) {
+            var value = document.createElement("div");
+
+            value.classList.add("value");
+            value.innerHTML = binomial(n, k);
+
+            row.appendChild(value);
+        }
+    }
+}
+
+function factorial(n) {
+    if (n == 0) return 1;
+
+    product = n;
+
+    for (var i = n - 1; i >= 1; i--) {
+        product *= i;
+    }
+
+    return product;
+}
+
+function binomial(n, k) {
+    return (factorial(n)) / (factorial(k) * factorial(n - k));
+}
