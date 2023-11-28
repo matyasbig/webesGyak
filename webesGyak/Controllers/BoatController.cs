e0725133df8def130582f41e92c60f13cf6cb8fd
xnawae <matyas.verseghi-nagy@stud.uni-corvinus.hu>
2023-11-28 10:26:13 +0100

    kérdések kiíratása

---
+++ webesGyak/Controllers/BoatController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webesGyak.Models;
+
+namespace webesGyak.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class BoatController : ControllerBase
+    {
+        [HttpGet]
+        [Route("questions/all")]
+        public IActionResult GetQuestions()
+        {
+            HajosContext context = new();
+            var questions = from x in context.Questions select x.Question1;
+
+            return Ok(questions);
+        }
+    }
+}
