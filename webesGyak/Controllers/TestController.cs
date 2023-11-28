5a60c1c1d22b651e6f12b21ccac32e477b0e9296
xnawae <matyas.verseghi-nagy@stud.uni-corvinus.hu>
2023-11-28 10:16:10 +0100

    TestController hozzáadása

---
+++ webesGyak/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webesGyak.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        [Route("corvinus/szerverido")]
+        public IActionResult M1()
+        {
+            string time = DateTime.Now.ToShortTimeString();
+
+            return new ContentResult
+            {
+                ContentType = System.Net.Mime.MediaTypeNames.Text.Plain,
+                Content = time
+            };
+        }
+
+        [HttpGet]
+        [Route("corvinus/nagybetus/{szoveg}")]
+        public IActionResult M2(string szoveg)
+        {
+            return Ok(szoveg.ToUpper());
+        }
+    }
+}
